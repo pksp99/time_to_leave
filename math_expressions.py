7f16485ea534afa04683b062eebfe16fb35e0a34
Preet Karia <karia.preet5@gmail.com>
2024-07-30 15:03:12 -0400

    Optimized evaluation of average cost.

--- math_expressions.py
+++ math_expressions.py
@@ -1,13 +1,6 @@
-from sympy import symbols, sympify, E, integrate, gamma, oo, lambdify, Eq, solve
+from sympy import symbols, sympify, E, integrate, gamma, oo, lambdify, lowergamma, uppergamma, simplify
 import numpy as np
 import plotly.graph_objs as go
-
-def lower_incomplete_gamma(s, x ='x'):
-    x = sympify(x)
-    s = sympify(s)
-    t = symbols('t')
-    expr = (t ** (s - 1)) * (E ** (-t))
-    return  integrate(expr, (t, 0, x))
 
 
 def gamma_pdf(alpha, beta, x='x'):
@@ -22,7 +15,7 @@ def gamma_cdf(alpha, beta, x='x'):
     alpha = sympify(alpha)
     beta = sympify(beta)
     x = sympify(x)
-    return lower_incomplete_gamma(alpha, x / beta) / gamma(alpha)
+    return lowergamma(alpha, x / beta) / gamma(alpha)
 
 
 def gamma_hazard_rate(alpha, beta, x='x'):
@@ -30,24 +23,30 @@ def gamma_hazard_rate(alpha, beta, x='x'):
     beta = sympify(beta)
     return gamma_pdf(alpha, beta) / (1 - gamma_cdf(alpha, beta))
 
+
+
 def gamma_cost(alpha, beta, h, c, x='x'):
     alpha = sympify(alpha)
     beta = sympify(beta)
     h = sympify(h)
     c = sympify(c)
     x = sympify(x)
-    t = sympify('t')
-    expr = integrate(t * gamma_pdf(alpha, beta, x=t), (t, x, oo))
-    print(expr)
+
+    # Simplified the integral for performance efficiency
+    expr = beta * uppergamma(alpha + 1, x / beta) / gamma(alpha)
     return c * gamma_cdf(alpha, beta) - x * h * (1 - gamma_cdf(alpha, beta)) + h * expr
 
 
 def plot_expression(expression, x_range, title, x_label, y_label, x='x'):
     x = sympify(x)
-    expr_func = lambdify(x, expression, 'numpy')
+    expression = simplify(expression)
     x_values = np.linspace(x_range[0], x_range[1], 400)
-    y_values = expr_func(x_values)
-    
+
+    # expr_func = lambdify(x, expression, 'numpy')
+    # y_values = expr_func(x_values)
+  
+    y_values = [float(expression.subs(x, x_val).evalf()) for x_val in x_values]
+
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=x_values, y=y_values, mode='lines', name=title))
     fig.update_layout(title=title, xaxis_title=x_label, yaxis_title=y_label)
